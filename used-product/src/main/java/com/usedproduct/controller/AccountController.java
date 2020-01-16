abb08f7e618582c734041b9d4580371c280eeaf4
mlopp <mlopp@LAPTOP-AB3072DF>
2020-01-16 17:16:15 +0900

    전규언 동네 설정 추가

--- used-product/src/main/java/com/usedproduct/controller/AccountController.java
+++ used-product/src/main/java/com/usedproduct/controller/AccountController.java
@@ -1,10 +1,26 @@
 package com.usedproduct.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.usedproduct.service.MemberService;
+import com.usedproduct.vo.MemberVO;
 
 @Controller
 @RequestMapping(path = {"/account/"})
 public class AccountController {
+	@Autowired
+	@Qualifier("memberService")
+	private MemberService memberService;
 	
+	@PostMapping(path = { "/register" })
+	public String register(MemberVO member) {
+		
+		memberService.registerMember(member);
+		
+		return "redirect:/resources/login.html";
+	}
 }
